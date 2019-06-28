c1d58cd4ea59e62814be010b1aa086199607a847
Your Name <administrator@CN-7.lan>
2019-06-28 09:46:28 +0100

    Updates

---
+++ precache-manifest.ff64eb3a3629a97ff54720d9992924be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ceb70b3b374f4bb9452cb1c1941b30",
+    "url": "/Nethalin/addressbook.git/index.html"
+  },
+  {
+    "revision": "00a47c0ae86e48fe1476",
+    "url": "/Nethalin/addressbook.git/static/css/main.64d7cedc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Nethalin/addressbook.git/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "00a47c0ae86e48fe1476",
+    "url": "/Nethalin/addressbook.git/static/js/main.22778682.chunk.js"
+  },
+  {
+    "revision": "6bb5de6770d43e155221",
+    "url": "/Nethalin/addressbook.git/static/js/runtime~main.6e5587c5.js"
+  }
+]);
